1f9856a64dec83aa4c8d7395cbc5d9cb44f605a8
adambiedrzycki <abiedrzycki@student.agh.edu.pl>
2022-11-17 17:56:00 +0100

    a

--- src/Ksiazka.java
+++ src/Ksiazka.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Ksiazka {
+public class Ksiazka {
+    private String Title;
+    private String Genre;
+    private int releaseYear;
+    private int idNumber;
+
+    //Konstruktor
+    Ksiazka(String Title, String Genre, int releaseYear, int identification){
+        this.Title = Title;
+        this.Genre = Genre;
+        this.releaseYear = releaseYear;
+        this.idNumber = identification;
+        //komentarzAdam
+    }
 }
